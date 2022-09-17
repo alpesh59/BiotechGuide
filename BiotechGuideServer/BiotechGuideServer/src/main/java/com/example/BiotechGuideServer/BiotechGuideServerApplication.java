fd4bbd8521c7eeb1de3d638b55675e823b93e092
Alpesh Makwana <agmakwana5959@gmail.com>
2022-09-17 11:11:21 +0530

    first commit

---
+++ BiotechGuideServer/BiotechGuideServer/src/main/java/com/example/BiotechGuideServer/BiotechGuideServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.BiotechGuideServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiotechGuideServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiotechGuideServerApplication.class, args);
+	}
+
+}
---
+++ BiotechGuideServer/BiotechGuideServer/src/test/java/com/example/BiotechGuideServer/BiotechGuideServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BiotechGuideServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiotechGuideServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
